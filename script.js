92cb6806c14dcd23fafb6011a94152b6aabcf77c
Paige Andrews <pandrews25@yahoo.com>
2020-04-30 17:40:10 -0400

    changed innerHTML out of code and replaced for all elements

--- script.js
+++ script.js
@@ -3,35 +3,53 @@ let questionSection = document.getElementById("questionSection");
 let questionContainer = document.getElementById("questionContainer");
 let subQ = document.getElementById("subQ");
 let mainQ = document.getElementById("mainQ");
-let choices = document.getElementsByClassName("choices");
 let choice1 = document.getElementById("choice1");
 let choice2 = document.getElementById("choice2");
 let choice3 = document.getElementById("choice3");
 let resultsHeader =document.getElementById("resultsHeader")
 let resultsContainer = document.getElementById("resultsContainer");
 let animalInformation = document.getElementById("animalInformation");
+let animalName = document.getElementById("animalName");
+let animalParagraph = document.getElementById("animalParagraph");
 let nextButton = document.getElementById("nextButton");
 let back = document.getElementById("back");
-let medToHave = document.getElementsByClassName("medToHave");
-let easyToHave = document.getElementsByClassName("easyToHave");
+let cat = document.getElementById("cat");
 let fish = document.getElementById("fish");
 let ferret = document.getElementById("ferret");
 let LargeDog = document.getElementById("largeDog");
 let goat = document.getElementById("goat");
 let largeBird = document.getElementById("largeBird");
 let horse = document.getElementById("horse");
+let medToHave = document.getElementsByClassName("medToHave");
+let easyToHave = document.getElementsByClassName("easyToHave");
+let choices = document.getElementsByClassName("choices");
+
+
+// window.onload = function() {
+//   var node = document.createElement("LI");
+//   var textnode = document.createTextNode("Water");
+//   node.appendChild(textnode);
+//   document.getElementById("myList").appendChild(node);
+
+//   mainQ.innerHTML = titleArray[0]
+//   subQ.innerHTML= subTitle[0]
+//   choice1.innerHTML = choice1Array[0]
+//   choice2.innerHTML = choice2Array[0]
+//   choice3.innerHTML = choice3Array[0]
+// };
+
 
 // Animal values for refference
 const animalObject = {
-cat:16,
-smallDog:16,
-largeDog:24,
-smallPet:-24,
-smallBird:-24,
-largeBird:30,
-reptile:-24,
-amphibian:-24,
-fish:-40,
+cat : 16,
+smallDog : 16,
+largeDog : 24,
+smallPet : -24,
+smallBird : -24,
+largeBird : 30,
+reptile : -24,
+amphibian : -24,
+fish : -40,
 Pig:16,
 ferret:0,
 goat:8,
@@ -39,11 +57,10 @@ horse:36
 }
 
 
-let nextClicked = 1;
+let nextClicked = 0;
 let petPoints = 0;
 
 let titleArray = [
-  "How do you feel about pet maintenance?",
   "If you have children, how will having a pet affect them?",
   "Will you want a pet that is less destructive to the home or outside area?",
   "How much space do you have inside and outside your home?",
@@ -53,8 +70,7 @@ let titleArray = [
   "Do you have a secondary caregiver?"
 ];
 
-let subTitle = [
-"Will you be able to live with pet hair, a litter box, vomit, water changes, general pet maintenance", 
+let subTitle = [ 
 "Is that animal safe around small children?",
 "If you rent, do you have permission from the property owner?",
 "Will you need a fenced yard?",
@@ -65,7 +81,6 @@ let subTitle = [
 ];
 
 let choice1Array= [
-"I don't want to deal with a lot of animal maintenance",
 "I have small children's saftely to consider",
 "I don't want a pet that can leave marks on the home whatsoever",
 "I only have enough room for a pet to stay stationary(mostly) in one room",
@@ -76,7 +91,6 @@ let choice1Array= [
 ];
 
 let choice2Array = [
-"I don't mind mild animal maintenance",
 "I have small children but I've taught them how to behave around animals",
 "I don't mind having the occational wear-and-tear caused by pets",
 "I have room for a pet to roam free in the home or some room outdoors",
@@ -87,7 +101,6 @@ let choice2Array = [
 ]
 
 let choice3Array = [
-"I don't mind a great deal of animal maintenance",
 "I dont have small children and we want a new family member",
 "Doesn't matter to me at all about damage caused by pets",
 "I have a large outside area for them",
@@ -97,37 +110,35 @@ let choice3Array = [
 "I have someone else in the home willing to take care of the pet anytime"
 ];
 
-let addDarkColor = ["darkBlue","darkAqua","darkPurple","darkGreen", "darkYellow", "darkOrange", "darkRed", "darkPink"];
-let addLightColor = ["lightBlue","lightAqua","lightPurple","lightGreen",  "lightYellow", "lightOrange", "lightRed", "lightPink"];
-let addButtonColor = ["blue-buttons","aqua-buttons","purple-buttons", "green-buttons", "yellow-buttons", "orange-buttons", "red-buttons", "pink-buttons"];
+let addDarkColor = ["darkAqua","darkPurple","darkGreen", "darkYellow", "darkOrange", "darkRed", "darkPink"];
+let addLightColor = ["lightAqua","lightPurple","lightGreen",  "lightYellow", "lightOrange", "lightRed", "lightPink"];
+let addButtonColor = ["aqua-buttons","purple-buttons", "green-buttons", "yellow-buttons", "orange-buttons", "red-buttons", "pink-buttons"];
 
-window.onload = function() {
-  mainQ.innerHTML = titleArray[0]
-  subQ.innerHTML= subTitle[nextClicked]
-  choice1.innerHTML = choice1Array[0]
-  choice2.innerHTML = choice2Array[0]
-  choice3.innerHTML = choice3Array[0]
-};
+let innerIds = [mainQ, subQ, choice1, choice2, choice3];
+let changingArrays = [titleArray, subTitle, choice1Array, choice2Array, choice3Array];
+let num = 0;  
+
 
  
 function changeHTML(){
-  mainQ.innerHTML = titleArray[nextClicked]
-  subQ.innerHTML= subTitle[nextClicked]
-  choice1.innerHTML = choice1Array[nextClicked]
-  choice2.innerHTML = choice2Array[nextClicked]
-  choice3.innerHTML = choice3Array[nextClicked]
+  for(i=0; i<5; i++){
+    innerIds[i].removeChild(innerIds[i].childNodes[0]); 
+    var textnode = document.createTextNode((changingArrays[i])[nextClicked]);
+    console.log(i)
+    innerIds[i].appendChild(textnode);
+  }
+  num++
+
   questionContainer.classList.add(addDarkColor[nextClicked])
-  questionSection.classList.add(addLightColor[nextClicked])
-  // nextButton.classList.add(addDarkColor[nextClicked])  
+  questionSection.classList.add(addLightColor[nextClicked])  
   choices[0].classList.add(addButtonColor[nextClicked])
   choices[1].classList.add(addButtonColor[nextClicked])
   choices[2].classList.add(addButtonColor[nextClicked])
   nextClicked++
-  if(nextClicked == 9){
+  if(nextClicked == 8){
         resultsFunction()
         resultsHeader.scrollIntoView()
     }
-
 }
 
 function resultsFunction(){
@@ -180,19 +191,19 @@ function resultsFunction(){
 choice1.addEventListener("click", function(){
   changeHTML()
   petPoints -= 5
- console.log(petPoints)
+//  console.log(petPoints)
 })
 
 choice2.addEventListener("click", function(){
   changeHTML()
   petPoints += 3
- console.log(petPoints)
+//  console.log(petPoints)
 })
 
 choice3.addEventListener("click", function(){
   changeHTML()
   petPoints += 5
- console.log(petPoints)
+//  console.log(petPoints)
 })
 
 // let animalIds = ["cat"];
@@ -201,3 +212,36 @@ choice3.addEventListener("click", function(){
 //  cat.addEventListener("click", function(){
 //   document.getElementById("1").scrollIntoView()
 //  })
+
+let animalNameArray = [cat, smallDog];
+
+let animalParagraphArray = [
+  "Cats are a very common household pet. They cannot stay cooped up in one room, and ideally need roam of the house or outside. Cat need a litter box (changed daily). If you want mulitiple cats you need one per cat with one extra. Cats enjoy scratching posts and high places. Please note that cats can have mild destruction on home when it comes to their claws and fur. They are very loving but also independent animals, so you won't have to worry about them at work if you have a busy schedule.",
+  "Small dogs can stay indoors or outdoors. They need regular exercise and do not cause much home destruction. They need attention and playtime though, so before adopting one you have sufficent time to spend with it daily. Small dogs really enjoy little play toys you can leave around your home for them to play with. They are not a high maintenance pet. "
+];
+
+// for (let name of animalNameArray){
+//   name.addEventListener("click", function(){
+//     animalParagraph.innerHTML = animalParagraphArray[animalNameArray.indexOf(name)]
+//   })
+// }
+
+// let test_object = {
+//   cat: [
+//     "nyName",
+//     "myDescription"
+//     "new"
+//     "new"
+//   ],
+//   dog:[
+//     "name",
+//     "description"
+//   ]
+// }
+
+// for (let name of "whatever the way to write object for loops is"){
+//   object_key.addEventListener("click", function(
+//     animalTitle.innerHTML = object_key[0]
+//     animalDescriptions.innerHTML = object_key[1] 
+//   ))
+// }
